9edf0cba784d6d6a118e221b754dae48da7d2916
Данила Мандрейкин <dan.mandreikin@yandex.ru>
2023-10-02 12:32:29 +0300

    FIX IMPORTS.

--- backend/api/views.py
+++ backend/api/views.py
@@ -8,8 +8,6 @@ from rest_framework.decorators import action
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework import filters, status, viewsets
-from rest_framework.permissions import IsAuthenticated
-from rest_framework.response import Response
 
 from api.filters import RecipeFilter
 from api.pagination import CustomPaginator
