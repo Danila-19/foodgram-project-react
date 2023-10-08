c69f9c93e78947bc324f8b742a9eb35e71305451
Данила Мандрейкин <dan.mandreikin@yandex.ru>
2023-10-08 17:58:34 +0300

    Fix rewies1.

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -94,7 +94,7 @@ class ShowingRecipeSerializer(serializers.ModelSerializer):
     tags = TagSerializer(many=True, read_only=True)
     ingredients = RecipeIngredientSerializer(
         many=True,
-        source='ingredient_recipe_ingredients')
+        source='recipe_ingredients')
     image = Base64ImageField(required=False, allow_null=True)
     is_favorited = serializers.SerializerMethodField()
     is_in_shopping_cart = serializers.SerializerMethodField()
@@ -137,7 +137,7 @@ def process_ingredients(self, recipe, ingredients):
 class RecipePostSerializer(ShowingRecipeSerializer):
 
     ingredients = RecipeIngredientSerializer(
-        source='ingredient_recipe_ingredients',
+        source='recipe_ingredients',
         many=True,)
     image = Base64ImageField(required=False, allow_null=True)
     author = ShowUserSerializer(read_only=True, required=False)
