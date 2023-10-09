79726805fd4a4e9abeb2cd2754eb4be46a7bf636
Данила Мандрейкин <dan.mandreikin@yandex.ru>
2023-10-09 18:39:51 +0300

    Fix subs.

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -255,7 +255,7 @@ class UserSubscribeSerializer(ShowUserSerializer):
 
     def validate(self, data):
         data = super().validate(data)
-        request = self.context['request']
+        request = self.context['request'].request
         author = self.instance
         user = request.user if request else None
         if user == author:
@@ -265,7 +265,7 @@ class UserSubscribeSerializer(ShowUserSerializer):
         return data
 
     def get_is_subscribed(self, obj):
-        request = self.context['request']
+        request = self.context['request'].request
         if request and not request.user.is_anonymous:
             return obj.follower.filter(user=request.user).exists()
         return False
