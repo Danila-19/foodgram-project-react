d6d83dfa131d0c8b9478a28e0702243cd8fcf9d3
Данила Мандрейкин <dan.mandreikin@yandex.ru>
2023-10-09 19:16:21 +0300

    Fix subs.

--- backend/api/pagination.py
+++ backend/api/pagination.py
@@ -4,7 +4,3 @@ from rest_framework.pagination import PageNumberPagination
 class CustomPaginator(PageNumberPagination):
     page_size_query_param = 'limit'
     ordering = ['id']
-
-
-class CustomPaginatorSubs(PageNumberPagination):
-    page_size_query_param = 'limit'
