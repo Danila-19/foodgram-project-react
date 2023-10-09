34443ab9ded184288f6d7de3857993e816bb6b2d
Данила Мандрейкин <dan.mandreikin@yandex.ru>
2023-10-09 19:42:06 +0300

    Fix subs.

--- backend/api/views.py
+++ backend/api/views.py
@@ -116,7 +116,7 @@ class IngredientViewSet(viewsets.ModelViewSet):
 
 
 class RecipeViewSet(viewsets.ModelViewSet):
-    queryset = Recipe.objects.all()
+    queryset = Recipe.objects.all().order_by('id')
     pagination_class = CustomPaginator
     filterset_class = RecipeFilter
     filter_backends = (DjangoFilterBackend, )
