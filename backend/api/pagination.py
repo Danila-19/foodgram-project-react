b9a066339a854ef7ccc0ebd71d4a8bc2beccb6ac
Данила Мандрейкин <dan.mandreikin@yandex.ru>
2023-10-11 14:33:40 +0300

    Fix recipes.

--- backend/api/pagination.py
+++ backend/api/pagination.py
@@ -3,4 +3,4 @@ from rest_framework.pagination import PageNumberPagination
 
 class CustomPaginator(PageNumberPagination):
     page_size_query_param = 'limit'
-    ordering = ['id']
+    ordering = ['-pub_date']
